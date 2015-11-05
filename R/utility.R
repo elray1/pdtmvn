3e6a023728d9552eea0cc5c0bbbcaf3d08d17a47
Evan Ray <evan.l.ray@gmail.com>
2015-11-05 16:25:10 -0500

    numerical utility functions to work in logspace.  starting using a couple of commits ago, but forgot to add then.

---
+++ R/utility.R
@@ -0,0 +1,34 @@
+### numerical functions
+
+## interface to R's C API for logspace arithmetic
+
+logspace_sub <- function(logx, logy) {
+	if(!identical(length(logx), length(logy))) {
+		stop("logx and logy must have the same length")
+	} else {
+		return(sapply(seq_along(logx), function(ind) {
+			return(.Call("logspace_sub_C", as.numeric(logx[ind]), as.numeric(logy[ind])))
+		}))
+	}
+}
+
+logspace_add <- function(logx, logy) {
+	if(!identical(length(logx), length(logy))) {
+		stop("logx and logy must have the same length")
+	} else {
+		return(sapply(seq_along(logx), function(ind) {
+			return(.Call("logspace_add_C", as.numeric(logx), as.numeric(logy)))
+		}))
+	}
+}
+
+logspace_sum_matrix_rows <- function(logX) {
+	return(.Call("logspace_sum_matrix_rows_C", as.numeric(logX), as.integer(nrow(logX)), as.integer(ncol(logX))))
+}
+
+logspace_sub_matrix_rows <- function(logX) {
+	if(!is.matrix(logX) || !identical(ncol(logX), 2L))
+		stop("logX must be a matrix with 2 columns")
+
+	return(.Call("logspace_sub_matrix_rows_C", as.numeric(logX), as.integer(nrow(logX))))
+}
